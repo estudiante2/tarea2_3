29f4f5e39e304be4c47030124720328cffdb1d88
tarea2-3 <estudiantesuth56@gmail.com>
2022-07-15 09:52:07 -0600

    Initial commit

---
+++ app/src/main/java/com/paredes/tarea2_3/MainActivity.java
@@ -0,0 +1,52 @@
+package com.paredes.tarea2_3;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+Button bguardar;
+    EditText descripcion;
+    Button btnCamara;
+    ImageView imgView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnCamara = findViewById(R.id.btnCamara);
+        bguardar = findViewById(R.id.bguardar);
+       descripcion = (EditText) findViewById(R.id.txtdescripcion);
+        imgView = findViewById(R.id.imageView);
+
+        btnCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirCamara();
+            }
+        });
+    }
+
+    private void abrirCamara(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            imgView.setImageBitmap(imgBitmap);
+        }
+    }
+}
